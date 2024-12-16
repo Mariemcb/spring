bb32802499d9cdd1746c96d82a21a2e1b8b50b51
syrine <syrinechelbi9@gmail.com>
2024-12-16 14:11:10 +0100

    jjjkkhh

--- src/main/java/com/example/AppController.java
+++ src/main/java/com/example/AppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class App {
+public class AppController {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
