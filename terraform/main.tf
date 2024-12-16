c8cd726025ea7dde06b5c9dc4fd0676df64f544b
syrine <syrinechelbi9@gmail.com>
2024-12-16 13:50:43 +0100

    jjjkk

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,31 @@
-# main.tf
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tpterraform-rg" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
-  
+  tags = {
+    Owner = var.owner
+    Script = "Custom Terraform Script"
+  }
 }
 
-resource "azurerm_app_service_plan" "tpterraform-serviceplan" {
-  name                = "tpterraformserviceplan"
-  location            = azurerm_resource_group.tpterraform-rg.location
-  resource_group_name = azurerm_resource_group.tpterraform-rg.name
+resource "azurerm_app_service_plan" "service-plan" {
+  name                = "app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku {
     tier = "Free"
     size = "F1"
   }
 }
 
-resource "azurerm_app_service" "tpterraform-appservice" {
-  name                = "tpterraformappservice"
-  location            = azurerm_resource_group.tpterraform-rg.location
-  resource_group_name = azurerm_resource_group.tpterraform-rg.name
-  app_service_plan_id = azurerm_app_service_plan.tpterraform-serviceplan.id
+resource "azurerm_app_service" "app-service" {
+  name                = "app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
   site_config {
     always_on = false
@@ -41,42 +42,60 @@ resource "azurerm_app_service" "tpterraform-appservice" {
   }
 }
 
-resource "azurerm_storage_account" "tpterraform-storageaccount" {
-  name                     = "examplestoracc${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.tpterraform-rg.name
-  location                 = azurerm_resource_group.tpterraform-rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    Owner = var.owner
-    Script = "Custom Terraform Script"
-  }
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-}
-
-resource "azurerm_storage_blob" "tpterraform-storgeblob" {
-  name                   = "hello-world.jar"
-  storage_account_name   = azurerm_storage_account.tpterraform-storageaccount.name
-  storage_container_name = azurerm_storage_account.tpterraform-storageaccount.primary_blob_container
-  type                   = "Block"
-  source                 = var.jar_file_path
-
-  depends_on = [
-    azurerm_storage_account.tpterraform-storageaccount
-  ]
-}
-
-resource "azurerm_storage_container" "tpterraform-storagecontainer" {
-  name                  = "example-container"
-  storage_account_name  = azurerm_storage_account.tpterraform-storageaccount.name
-  container_access_type = "private"
-}
-
 output "app_service_url" {
-  value = azurerm_app_service.tpterraform-appservice.default_site_hostname
+  value = azurerm_app_service.app-service.default_site_hostname
+}
+
+
+
+pipeline {
+  agent any
+
+  stages {
+    stage('Build') {
+      steps {
+        script {
+          sh "mvn clean package"
+        }
+      }
+    }
+
+    stage('Upload to Azure Storage') {
+      steps {
+        script {
+          sh "az storage blob upload --account-name ${storageAccountName} --container-name ${storageContainerName} --name hello-world.jar --file ${jarFile} --auth-mode login"
+        }
+      }
+    }
+
+    stage('Initialize Terraform') {
+      steps {
+        script {
+          runTerraform('init')
+        }
+      }
+    }
+
+    stage('Plan Terraform') {
+      steps {
+        script {
+          runTerraform('plan')
+        }
+      }
+    }
+
+    stage('Apply Terraform') {
+      steps {
+        script {
+          runTerraform('apply -auto-approve')
+        }
+      }
+    }
+  }
+
+  post {
+    always {
+      echo 'Pipeline complete.'
+    }
+  }
 }
